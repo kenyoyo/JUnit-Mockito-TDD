5e91a9378b7fb0bcf9163f20245401d42530ced4
kenyoyo <kendeb_2014@gmail.com>
2018-11-23 17:26:51 +0700

    CheckNumber True-False Testing

---
+++ true-false-test/src/main/java/com/kazdo/true_false_test/NumberCheck.java
@@ -0,0 +1,26 @@
+package com.kazdo.true_false_test;
+
+public class NumberCheck 
+{
+    public static boolean isNegative(int n) {
+    	if(n < 0)
+    		return true;
+    	else
+    		return false;
+    }
+    
+    public static boolean isPositive(int n) {
+    	if(n > 0)
+    		return true;
+    	else
+    		return false;
+    }
+    
+    public static boolean isZero(int n) {
+    	if(n == 0)
+    		return true;
+    	else
+    		return false;
+    }
+    
+}
