69ea43a9dc6944191e959913baebd136079191b9
kenyoyo <kendeb_2014@gmail.com>
2018-11-24 13:59:59 +0700

    Mutiply function parameterized testing

--- multiply-function/src/test/java/com/kazdo/multiply_function/TestMultiply.java
+++ multiply-function/src/test/java/com/kazdo/multiply_function/TestMultiply.java
@@ -16,28 +16,29 @@ public class TestMultiply {
 
 	private String input;
 	private String expectedOutput;
-	
+
 	public TestMultiply(String input, String expectedOutput) {
 		this.input = input;
 		this.expectedOutput = expectedOutput;
 	}
-	
+
 	@Parameters
 	public static Collection<String[]> testCondition() {
-		String expectedOutputs[][] = { {"5,2", "25"},
-									   {"10,5", "100000"},
-									   {"9,3","729"} };
-		
+		String expectedOutputs[][] = { 
+				{ "5 2", "25" }, 
+				{ "10 5", "100000" }, 
+				{ "9 3", "729" } };
+
 		return Arrays.asList(expectedOutputs);
 	}
-	
+
 	@Test
 	public void testMultiplyPowFunction() {
-		String splitInput[] = input.split(",");
+		String splitInput[] = input.split(" ");
 		int number = Integer.parseInt(splitInput[0]);
 		int power = Integer.parseInt(splitInput[1]);
 		int castExpectedOutput = Integer.parseInt(expectedOutput);
-		
+
 		assertEquals(castExpectedOutput, Multiply.pow(number, power));
 	}
 }
