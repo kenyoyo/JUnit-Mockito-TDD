63b210d63f16384fd7dc6c019f0e1bd8344dfc3d
kenyoyo <kendeb_2014@gmail.com>
2018-11-24 12:10:24 +0700

    Unit Testing

--- array-builder/src/test/java/com/kazdo/array_equal_test/ArrayBuilderTest.java
+++ array-builder/src/test/java/com/kazdo/array_equal_test/ArrayBuilderTest.java
@@ -10,7 +10,7 @@ public class ArrayBuilderTest {
 	
 	@Test
 	public void testArrayBuilderIntegerArray() {
-		arb = new ArrayBuilder<Integer>();
+		arb = new ArrayBuilder();
 		Integer[] expectedArray = {1, 3, 4, 5};
 		arb.build(1, 3, 4, 5);
 		
@@ -19,7 +19,7 @@ public class ArrayBuilderTest {
 	
 	@Test
 	public void testArrayBuilderStringArray() {
-		arb = new ArrayBuilder<String>();
+		arb = new ArrayBuilder();
 		String[] expectedArray = {"A", "B", "C", "D"};
 		arb.build("A", "B", "C", "D");
 
@@ -28,16 +28,22 @@ public class ArrayBuilderTest {
 	
 	@Test
 	public void testArrayBuilderDecimalArray() {
-		arb = new ArrayBuilder<Double>();
+		arb = new ArrayBuilder();
 		Double[] expectedArray = {10.2, 3.14, 1.999, 77.77};
 		arb.build(10.2, 3.14, 1.999, 77.77);
 		
 		assertArrayEquals(expectedArray, arb.get());
 	}
 	
-	@Test
-	public void testArrayBuilderUnexpectedType() {
-		ArrayBuilder<String> arb = new ArrayBuilder<String>();
-		arb.build(99, 199);
+	@Test(expected=NullPointerException.class)
+	public void testArrayBuilderThrowException() {
+		arb.build("Need to declare object for this variable");
+	}
+	
+	@Test(timeout=10)
+	public void testArrayBuilderTimeout() {
+		arb = new ArrayBuilder();
+		for(int i=0; i<1000000; i++)
+			arb.build(i);
 	}
 }
