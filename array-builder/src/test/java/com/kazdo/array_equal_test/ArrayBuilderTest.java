13b3b632bd3d1a8213ab157d115e99db497dad56
kenyoyo <kendeb_2014@gmail.com>
2018-11-24 12:00:26 +0700

    JUnit Testing

--- array-builder/src/test/java/com/kazdo/array_equal_test/ArrayBuilderTest.java
+++ array-builder/src/test/java/com/kazdo/array_equal_test/ArrayBuilderTest.java
@@ -9,7 +9,7 @@ public class ArrayBuilderTest {
 	ArrayBuilder arb;
 	
 	@Test
-	public void testBuilderIntegerArray() {
+	public void testArrayBuilderIntegerArray() {
 		arb = new ArrayBuilder<Integer>();
 		Integer[] expectedArray = {1, 3, 4, 5};
 		arb.build(1, 3, 4, 5);
@@ -18,7 +18,7 @@ public class ArrayBuilderTest {
 	}
 	
 	@Test
-	public void testBuilderStringArray() {
+	public void testArrayBuilderStringArray() {
 		arb = new ArrayBuilder<String>();
 		String[] expectedArray = {"A", "B", "C", "D"};
 		arb.build("A", "B", "C", "D");
@@ -27,11 +27,17 @@ public class ArrayBuilderTest {
 	}
 	
 	@Test
-	public void testBuilderDecimalArray() {
+	public void testArrayBuilderDecimalArray() {
 		arb = new ArrayBuilder<Double>();
 		Double[] expectedArray = {10.2, 3.14, 1.999, 77.77};
 		arb.build(10.2, 3.14, 1.999, 77.77);
 		
 		assertArrayEquals(expectedArray, arb.get());
+	}
+	
+	@Test
+	public void testArrayBuilderUnexpectedType() {
+		ArrayBuilder<String> arb = new ArrayBuilder<String>();
+		arb.build(99, 199);
 	}
 }
